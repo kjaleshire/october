b436a907fa47e709dd464594d140735989353697
Kyle J Aleshire <kjaleshire@gmail.com>
2012-10-10 16:34:50 -0800

    Minor comment & licensing updates

--- october.h
+++ october.h
@@ -5,6 +5,7 @@ Simple threaded HTTP server. Read GET replies from a host and respond appropriat
 Main header file
 
 (c) 2012 Kyle J Aleshire
+All rights reserved
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
